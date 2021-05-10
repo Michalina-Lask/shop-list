a793a2b58121bba29f01173088d72ddcc63d8132
m.laskowska <milaskowska18@gmail.com>
2021-05-10 20:27:10 +0200

    change numberx

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-form ref="form">
+      <v-text-field
+        label="Shop"
+        :rules="rules"
+        hide-details="auto"
+        v-model="shop"
+      ></v-text-field>
+      <v-text-field
+        label="Calories"
+        :rules="rules"
+        hide-details="auto"
+        v-model="calories"
+      ></v-text-field>
+      <v-text-field
+        label="Fat"
+        :rules="rules"
+        hide-details="auto"
+        v-model="fat"
+      ></v-text-field>
+
+            <v-btn
+        
+        color="success"
+        class="mr-4"
+        @click="addProduct"
+      >
+        Dodaj Produkt
+      </v-btn>
+    </v-form>
+
     <v-data-table
       :headers="headers"
       :items="desserts"
@@ -18,13 +48,24 @@ export default {
           text: "Shop (100g serving)",
           align: "start",
           sortable: false,
-          value: "name",
+          value: "shop",
         },
         { text: "Calories", value: "calories" },
         { text: "Fat (g)", value: "fat" },
       ],
       desserts: [],
+      shop:"",
+      calories:"",
+      fat:""
     };
   },
+  methods: {
+    addProduct(){
+      this.desserts.push({shop:this.shop,calories:this.calories, fat:this.fat})
+      
+    }
+    
+  }
+ 
 };
 </script>
