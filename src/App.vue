2adee4fee5e8c8ad8633b4ed19ab709e3bef96ea
m.laskowska <milaskowska18@gmail.com>
2021-05-16 15:45:14 +0200

    change6

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <v-app id="inspire">
-    
     <v-row>
       <v-col cols="4">
-        <add-button></add-button>
-        <!-- <v-btn color="success" class="mr-4" @click="addProduct">
-        Dodaj Produkt
-      </v-btn> -->
+        <v-btn color="success" class="mr-4" @click="openForm">
+          Dodaj Produkt
+        </v-btn>
       </v-col>
       <v-col cols="12">
         <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
       </v-col>
     </v-row>
-    <dialog-window :dialog="dialog"></dialog-window>
-
-   
+    <dialog-window
+      :dialog="dialog"
+      @update-dialog="updateDialog"
+      @add-product="addProduct"
+    ></dialog-window>
   </v-app>
 </template>
 
 <script>
-import DialogWindow from './components/Dialog Window'
-import AddButton from './components/AddButton'
-
-
+import DialogWindow from "./components/DialogWindow";
 
 export default {
   data() {
@@ -43,43 +40,30 @@ export default {
         { text: "Fat (g)", value: "fat" },
       ],
       desserts: [],
-      shop: "",
-      calories: "",
-      fat: "",
-      dialog: true
+      
+      dialogVisible: false,
     };
   },
   components: {
-        DialogWindow,
-        AddButton,
-    
-   
+    DialogWindow,
+  },
+  computed: {
+    dialog() {
+      return this.dialogVisible;
+    },
   },
   methods: {
-    addProduct() {
-      this.dialog = true
-      /* let obj = { shop: this.shop, calories: this.calories, fat: this.fat };
+    updateDialog(val) {
+      this.dialogVisible = val;
+    },
+    addProduct(obj) {
+    
 
-      this.desserts.push(obj); */
+      this.desserts.push(obj);
     },
-    setVisibleAlert(val) {
-      if (val.length > 0) {
-        this.alertVisibility = false;
-      } else {
-        this.alertVisibility = true;
-      }
+    openForm() {
+      this.dialogVisible = true;
     },
-  },
-  watch: {
-    shop(val) {
-      this.setVisibleAlert(val)
-    },
-    calories(val){
-      this.setVisibleAlert(val)
-    },
-    fat(val){
-      this.setVisibleAlert(val)
-    }
   },
 };
 </script>
---
+++ src/components/DialogWindow.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4"> </v-col>
+              <v-col cols="12" sm="6" md="4"> </v-col>
+              <v-col cols="12" sm="6" md="4"> </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="shop" label="Shop" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="calories" label="Calories" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select v-model="fat"
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Fat"
+                  required
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red" text @click="closeDialog">
+            Close
+          </v-btn>
+          <v-btn color="green" text @click="saveData">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ["dialog"],
+  data() {
+    return {
+      shop: "",
+      calories: "",
+      fat: "",
+    };
+  },
+  methods: {
+    saveData() {
+      let obj = { shop: this.shop, calories: this.calories, fat: this.fat }
+      this.$emit("add-product", obj) 
+      this.closeDialog()
+    },
+    closeDialog() {
+      this.resetForm()
+      this.dialog = false
+      this.$emit("update-dialog", this.dialog);
+    },
+    resetForm(){
+      this.shop = "",
+      this.calories = "",
+      this.fat = ""
+    }
+  },
+};
+</script>
+
+<style></style>
